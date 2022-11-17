34b502eca4f1dad67b77a6604c9d89b0255e8f44
Mikhail Zelenchuk <zelmisha2@yandex.ru>
2022-11-17 15:20:23 +0300

    Added docstring to main.py

--- main.py
+++ main.py
@@ -1,39 +1,53 @@
 from random import randint
 
+from graphic_arts.start_game_banner import run_screensaver
+
 
 def attack(char_name: str, char_class: str) -> str:
-    damage: int = 0
+    """Атака по противнику.
+    Герой, в зависимости от класса, наносит разный урон.
+    Возвращает сообщение с кол-вом нанесённого урона.
+    """
+    damage: int = 5
     if char_class == 'warrior':
-        damage = 5 + randint(3, 5)
+        damage += randint(3, 5)
         return f'{char_name} нанёс урон противнику равный {damage}'
     if char_class == 'mage':
-        damage = 5 + randint(5, 10)
+        damage += randint(5, 10)
         return f'{char_name} нанёс урон противнику равный {damage}'
     if char_class == 'healer':
-        damage = 5 + randint(-3, -1)
+        damage += randint(-3, -1)
         return f'{char_name} нанёс урон противнику равный {damage}'
     return f'{char_name} не нанёс урон'
 
 
 def defence(char_name: str, char_class: str) -> str:
-    blocked_damage: int = 0
+    """Блокируется урона противника.
+    Герой блокирует урон от противника по разному в зависимости от класса.
+    Возвращает сообщение о блокированом уроне.
+    """
+    blocked_damage: int = 10
     if char_class == 'warrior':
-        blocked_damage = 10 + randint(5, 10)
+        blocked_damage += randint(5, 10)
         return f'{char_name} блокировал {blocked_damage} урона'
     if char_class == 'mage':
-        blocked_damage = 10 + randint(-2, 2)
+        blocked_damage += randint(-2, 2)
         return f'{char_name} блокировал {blocked_damage} урона'
     if char_class == 'healer':
-        blocked_damage = 10 + randint(2, 5)
+        blocked_damage += randint(2, 5)
         return f'{char_name} блокировал {blocked_damage} урона'
     return f'{char_name} не блокировал урон'
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Использование спеуиального умения.
+    Применение различных умений разных классов.
+    Возвращает сообщение о применении спецприёма.
+    """
     skill: int = 0
     if char_class == 'warrior':
         skill = 80 + 25
-        return (f'{char_name} применил специальное умение'
+        return (f'{char_name} применил специальное умение '
                 f'«Выносливость {skill}»')
     if char_class == 'mage':
         skill = 5 + 40
@@ -45,6 +59,10 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Тренеровка для игрока.
+    Предлагает использовать разные навыки.
+    Выводит результаты работы функций attack, defence и special.
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -54,9 +72,9 @@ def start_training(char_name: str, char_class: str) -> str:
     print('Потренируйся управлять своими навыками.')
     print(
         'Введи одну из команд: '
-        'attack — чтобы атаковать противника,'
-        'defence — чтобы блокировать атаку противника или'
-        'special — чтобы использовать свою суперсилу.')
+        'attack — чтобы атаковать противника, '
+        'defence — чтобы блокировать атаку противника или '
+        'special — чтобы использовать свою суперсилу. ')
     print('Если не хочешь тренироваться, введи команду skip.')
     cmd: str = ''
     while cmd != 'skip':
@@ -71,6 +89,9 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Выбор героя.
+    Предлагает выбрать героия из списка: Воитель, Маг, Лекарь.
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
@@ -78,13 +99,13 @@ def choice_char_class() -> str:
             'Введи название персонажа, за которого хочешь играть:'
             'Воитель — warrior, Маг — mage, Лекарь — healer: ')
         if char_class == 'warrior':
-            print('Воитель — дерзкий воин ближнего боя.'
+            print('Воитель — дерзкий воин ближнего боя. '
                   'Сильный, выносливый и отважный.')
         if char_class == 'mage':
-            print('Маг — находчивый воин дальнего боя.'
+            print('Маг — находчивый воин дальнего боя. '
                   'Обладает высоким интеллектом.')
         if char_class == 'healer':
-            print('Лекарь — могущественный заклинатель.'
+            print('Лекарь — могущественный заклинатель. '
                   'Черпает силы из природы, веры и духов.')
         approve_choice = input(
             'Нажми (Y), чтобы подтвердить выбор, '
@@ -93,7 +114,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main() -> None:
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -103,6 +125,3 @@ def main() -> None:
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
--- temp.py
+++ temp.py
@@ -1,40 +1,25 @@
-# Тестовые данные.
-TEST_DATA: list[tuple[int, str, bool]] = [
-    (44, 'success', True),
-    (16, 'failure', True),
-    (4, 'success', False),
-    (21, 'failure', False),
-]
+from math import sqrt
 
-BONUS: float = 1.1
-ANTIBONUS: float = 0.8
+message = ('Добро пожаловать в самую лучшую программу для вычисления '
+           'квадратного корня из заданного числа!')
+
+print(message)
 
 
-def add_rep(current_rep: float, rep_points: int, buf_effect: bool) -> float:
-    current_rep += rep_points
-    if buf_effect:
-        return current_rep * BONUS
-    return current_rep
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
 
 
-def remove_rep(current_rep: float, rep_points: int,
-               debuf_effect: bool) -> float:
-    current_rep -= rep_points
-    if debuf_effect:
-        return current_rep * ANTIBONUS
-    return current_rep
+def calc(your_number):
+    """Проверят возможность извлечения корня и выводит результат."""
+
+    if your_number < 0:
+        return 'Число не должно быть меньше нуля'
+
+    root = calculate_square_root(your_number)
+    return (f'Мы вычислили квадратный корень из введённого вами числа. '
+            f'Это будет: {root}')
 
 
-def main(duel_res: list[tuple[int, str, bool]]) -> str:
-    current_rep: float = 0.0
-    for rep, result, effect in duel_res:
-        if result == 'success':
-            current_rep = add_rep(current_rep, rep, effect)
-        if result == 'failure':
-            current_rep = remove_rep(current_rep, rep, effect)
-    return (f'После {len(duel_res)} поединков, репутация персонажа — '
-            f'{current_rep:.3f} очков.')
-
-
-# Тестовый вызов функции main.
-print(main(TEST_DATA))
+print(calc(25.5))
